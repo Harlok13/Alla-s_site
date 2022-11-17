cbd3b90d221ca8f06eb14f7736f8122e3e9f0fd0
Harlok <holmik90@yandex.ru>
2022-11-17 21:37:43 +0300

    add model Category

--- alla_site/main/models.py
+++ alla_site/main/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 class Posts(models.Model):
+    """Посты"""
     title = models.CharField(max_length=200, verbose_name='Название')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+    slug = models.SlugField(max_length=200, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(verbose_name='Текст статьи')
     photo = models.ImageField(upload_to='archive/%Y/%m/%d', verbose_name='Фото')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категории')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категории')
 
     def __str__(self):
         return self.title
@@ -17,4 +18,23 @@ class Posts(models.Model):
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
         ordering = ['time_create', 'title']
+
+
+    class Category(models.Model):
+        """Категории"""
+        name = models.CharField(max_length=200, verbose_name='Название')
+        slug = models.SlugField(max_length=200, unique=True, verbose_name='URL')
+
+        def __str__(self):
+            return self.name
+
+        class Meta:
+            verbose_name = 'Категория'
+            verbose_name_plural = 'Категории'
+
+
+
+
+
+
 
